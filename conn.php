1bb584f665fb20e9e849666d3683ecd59d7a387a
jysquare <140070133+jysquare@users.noreply.github.com>
2025-03-20 03:16:24 +0800

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,6 +1,6 @@
 <?php
 // Change this to your own configuration
-$link = new mysqli('localhost','NAME','PASSWORD','kano');
+$link = new mysqli('120.26.115.188','root','200467','kano');
 mysqli_set_charset($link, 'utf8');
 if ($link->connect_error) {
     die("Failed to connect: " . $conn->connect_error);
